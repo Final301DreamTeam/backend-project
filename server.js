c6b5f80c605171eb2437f2f5feb34db1eda551a5
Darius <darius.owens.j@gmail.com>
2022-05-05 15:30:08 -0700

    updated auth0

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 3002;
 async function getRestaurants(request, response, next)
 {
     
-  authUser(request, async (error, user) =>{
+  verifyUser(request, async (error, user) =>{
     if(error) {
       console.error(error);
       response.send('token recieved is invalid, try again');
@@ -42,26 +42,17 @@ async function getRestaurants(request, response, next)
         
        const searchObject = {};
        if(request.query.email) searchObject.email = req.query.email;
-    }
     try{
       const restaurantFromDb = await Restaurant.find(searchObject);
       if(restaurantFromDb.length > 0)
         response.status(200).send()
-    } catch  (e){
-      console.error(e);
-      response.status(500).send('server error')
-    }
-  
-
-  
-    
 
       const userCity = request.query.location;
       const userInput = request.query.term;
-      const gAddress = request.query.address;
+      // const gAddress = request.query.address;
       const url = `https://api.yelp.com/v3/businesses/search?&limit=15&term=${userInput}&location=${userCity}&apiKey=${process.env.apiKey}`;
-      const googleRequest = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=601+E+Pike+St,+Unit+100,+Seattle,+WA+98122&key=${process.env.GAPI_KEY}`);
-      console.log("HERE ---->", googleRequest.data,"<--------HERE");
+      // const googleRequest = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=601+E+Pike+St,+Unit+100,+Seattle,+WA+98122&key=${process.env.GAPI_KEY}`);
+      // console.log("HERE ---->", googleRequest.data,"<--------HERE");
       let foodData = await axios.get(url, {
         headers:{
           'Authorization': `Bearer ${process.env.apiKey}`
@@ -74,8 +65,10 @@ async function getRestaurants(request, response, next)
     {
         next(error);
     }
-    //}});
+    }});
 }
+
+
 
 
 
@@ -138,7 +131,7 @@ app.get('/', (request, response) => {
 
 
 class RestaurantData {
-  constructor(rest, google)//objects with data
+  constructor(rest)//objects with data
   {
 
       //console.log("HERE", rest);
